040d1f6c91d73f28a6f4e6874765388e95f1fbc2
aw2389 <alex2@weghorst.com>
2021-06-19 16:25:55 -0500

    Fixed category_id POST bug

--- routes/api/product-routes.js
+++ routes/api/product-routes.js
@@ -69,7 +69,7 @@ router.post("/", (req, res) => {
     product_name: req.body.product_name,
     price: req.body.price,
     stock: req.body.stock,
-    categoryIds: req.body.category_ids,
+    category_id: req.body.category_id,
     tagIds: req.body.tagIds,
   })
     .then(product => {
